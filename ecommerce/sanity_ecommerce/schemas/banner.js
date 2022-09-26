1b4950a492924e44fb0f6333462c923adf78bbf7
Cesar Martinez Araujo <cesarmartinezaraujo@outlook.com>
2022-09-25 20:18:12 -0700

    Add sanity schemas
    -Add schema for products
    -Add Schema for banner

---
+++ ecommerce/sanity_ecommerce/schemas/banner.js
@@ -0,0 +1,66 @@
+/*
+    This is the banner schema. It defines the fields that will be used to create a banner.
+    The name of the schema is 'banner' and the title is 'Banner'.
+    Fieds: image, buttonText, product, description, middleText, largeText1, largeText2, discount, saleTime
+ */
+
+export default {
+    name: 'banner',
+    title: 'Banner',
+    type: 'document',
+    fields: [
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'image',
+            options: {
+                hotspot: true,
+            },
+        },
+        {
+            name: 'buttonText',
+            title: 'ButtonText',
+            type: 'string',
+        },
+        {
+            name: 'product',
+            title: 'Product',
+            type: 'string',
+        },
+        {
+            name: 'desc',
+            title: 'Desc',
+            type: 'string',
+        },
+        {
+            name: 'smallText',
+            title: 'SmallText',
+            type: 'string',
+        },
+        {
+            name: 'midText',
+            title: 'MidText',
+            type: 'string',
+        },
+        {
+            name: 'largeText1',
+            title: 'LargeText1',
+            type: 'string',
+        },
+        {
+            name: 'largeText2',
+            title: 'LargeText2',
+            type: 'string',
+        },
+        {
+            name: 'discount',
+            title: 'Discount',
+            type: 'string',
+        },
+        {
+            name: 'saleTime',
+            title: 'SaleTime',
+            type: 'string',
+        },
+    ],
+};
---
+++ ecommerce/sanity_ecommerce/schemas/product.js
@@ -0,0 +1,46 @@
+/*
+    This is the Product schema. It defines the fields that will be used to create a product.
+    The name of the schema is 'product' and the title is 'Product'.
+    Fieds: image, name, slug, price, details
+ */
+
+export default{
+    name: 'product',
+    title: 'Product',
+    type: 'document',
+    fields: [
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'array',
+            of: [{type: 'image'}],
+            options: {
+                hotspot: true,
+            }
+        },
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string'
+        },
+        {
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: {
+                source: 'name',
+                maxLength: 90
+            }
+        },
+        {
+            name: 'price',
+            title: 'Price',
+            type: 'number',
+        },
+        {
+            name: 'details',
+            title: 'Details',
+            type: 'string',
+        }
+    ]
+};
