b01b1f62ea8d0ab32ed1c8ed740c8ab6d6dbb43c
Cesar Martinez Araujo <cesarmartinezaraujo@outlook.com>
2022-09-29 22:22:36 -0700

    Implement FooterBanner Component

--- ecommerce/components/FooterBanner.jsx
+++ ecommerce/components/FooterBanner.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React from 'react';
 
-const FooterBanner = () => {
+import Link from 'next/link';
+import { urlFor } from '../lib/client';
+
+/*
+  This component is used to display the footer banner of the home page.
+  It takes in the footerBanner object following the schema defined for sanity.
+  Schema located in sanity/schemas/footerBanner.js
+ */
+const FooterBanner = ({footerBanner:{discount, largeText1, largeText2, saleTime, smallText,
+midText, desc, product, buttonText, image}}) => {
   return (
-    <div>FooterBanner</div>
+    <div className='footer-banner__container'>
+      <div className='banner__desc'>
+        <div className='left'>
+          <p>{discount}</p>
+          <h3>{largeText1}</h3>
+          <h3>{largeText2}</h3>
+          <p>{saleTime}</p>
+        </div>
+
+        <div className='right'>
+          <p>{smallText}</p>
+          <h3>{midText}</h3>
+          <p>{desc}</p>
+          <Link href={`/product/${product}`}>
+            <button type='button'>{buttonText}</button>
+          </Link>
+        </div>
+
+        <img src={urlFor(image)} className='footer-banner__image' alt="" />
+      </div>
+    </div>
   )
 }
 
