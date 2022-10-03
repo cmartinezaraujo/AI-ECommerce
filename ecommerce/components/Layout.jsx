d614eca2134fd26ef0c4dda6497dd20bd9d35ebf
Cesar Martinez Araujo <cesarmartinezaraujo@outlook.com>
2022-10-02 19:56:59 -0700

    Implement Layout component
    -Add layout styles

--- ecommerce/components/Layout.jsx
+++ ecommerce/components/Layout.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Head from 'next/head';
 
-const Layout = () => {
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const Layout = ({children}) => {
   return (
-    <div>Layout</div>
+    <div className='layout'>
+      <Head>
+        <title>ECommerce Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+
+      <main className='main__container'>
+        {children}
+      </main>
+
+      <footer>
+        <Footer />
+      </footer>
+    </div>
   )
 }
 
