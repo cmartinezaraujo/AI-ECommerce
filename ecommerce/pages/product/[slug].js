9151487761b20e34a9316c95ce17d6f22f2a9b61
Cesar Martinez Araujo <cesarmartinezaraujo@outlook.com>
2022-10-05 14:34:10 -0700

    Add product image preview and similar product section
    -Add preview images for product
    -Add preview to similar products on bottom of page

--- ecommerce/pages/product/[slug].js
+++ ecommerce/pages/product/[slug].js
@@ -1,21 +1,31 @@
-import React from 'react';
+import {React, useState} from 'react';
+
+import { Product } from '../../components';
 
 import { client, urlFor } from '../../lib/client';
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
 
 const ProductDetails = ({product, products}) => {
     const {image, name, details, price} = product;
-
+    const [index, setIndex] = useState(0);
   return (
     <div>
     <div className="product__detail-container">
       <div>
         <div className="image-container">
-          <img src={urlFor(image && image[0])} className="product__detail-image" />
+          <img src={urlFor(image && image[index])} className="product__detail-image" />
         </div>
-        {/* <div className="small-images-container">
-        
-        </div> */}
+        <div className="small-images__container">
+            {image?.map((img, i) => (
+                <img
+                    src={urlFor(img)}
+                    className={i === index ? 
+                        `small-image selected-image` : 
+                        `small-image`}
+                    onMouseEnter={() => setIndex(i)}
+                />
+            ))}
+        </div>
       </div>
 
       <div className="product__detail-desc">
@@ -48,6 +58,17 @@ const ProductDetails = ({product, products}) => {
           <button type="button" className="buy-now" onClick="">Buy Now</button>
         </div>
       </div>
+    </div>
+
+    <div className='maylike-products__wrapper'>
+        <h2>You may also like</h2>
+        <div className='marquee'>
+            <div className='maylike-products__container track'>
+                {products.map((item) => (
+                    <Product key={item._id} product={item}/>
+                ))}
+            </div>
+        </div>
     </div>
   </div>
   )
