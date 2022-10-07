dac0d63f467531fcee8479f53d8c77b26454cef2
Cesar Martinez Araujo <cesarmartinezaraujo@outlook.com>
2022-10-06 18:48:43 -0700

    Add cart logic
    -Add logic for inserting new items into the car
    -Add cart component to Navbar

--- ecommerce/context/StateContext.js
+++ ecommerce/context/StateContext.js
@@ -5,11 +5,59 @@ import {toast} from "react-hot-toast";
 const Context = createContext();
 
 export const StateContext = ({children}) => {
+    /*
+       Cart State
+       -showCart: boolean to show/hide cart
+       -cartItems: array of items in cart
+       -totalPrice: total price of items in cart
+       -totalItems: total number of items in cart
+       -qty: quantity of items in cart
+    */
     const [showCart, setShowCart] = useState(false);
-    const [cartItems, setcartItems] = useState();
+    const [cartItems, setcartItems] = useState([]);
     const [totalPrice, setTotalPrice] = useState();
-    const [totalQuantities, setTotalQuantities] = useState();
-    const [qty, setQty] = useState(1);
+    const [totalQuantities, setTotalQuantities] = useState(0);
+    const [quantity, setQuantity] = useState(1);
+
+    const onAdd= (product, quantity) => {
+        const checkProductInCart = cartItems.find((item) => item._id === product._id);
+
+        setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity);
+        setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
+
+        //if product is already in cart, increase quantity
+        if (checkProductInCart) {
+
+            const updatedCartItems = cartItems.map((cartProduct) => {
+                if (cartProduct._id === product._id) {
+                    return {
+                        ...cartProduct,
+                        quantity: cartProduct.quantity + quantity,
+                    };
+                }
+                return cartProduct;
+            });
+
+            setcartItems(updatedCartItems);
+        }else{
+            product.quantity = quantity;
+
+            setcartItems([...cartItems, product]);
+        }
+
+        toast.success(`${quantity} ${product.name} added to cart`);
+    }
+
+    const increaseQuantity = () => {
+        setQuantity(prevQty => prevQty + 1);
+    }
+
+    const decreaseQuantity = () => {
+        setQuantity(prevQty => {
+            if(prevQty-1 < 1) return 1;
+            return prevQty - 1
+        });
+    }
 
     return (
         <Context.Provider 
@@ -18,9 +66,17 @@ export const StateContext = ({children}) => {
                 cartItems, 
                 totalPrice,
                 totalQuantities,
-                qty,
+                quantity,
+                increaseQuantity,
+                decreaseQuantity,
+                onAdd,
+                showCart,
+                setShowCart,
+                totalQuantities,
             }}>
             {children}
         </Context.Provider>
     )
 }
+
+export const useStateContext = () => useContext(Context);
--- ecommerce/pages/product/[slug].js
+++ ecommerce/pages/product/[slug].js
@@ -4,10 +4,14 @@ import { Product } from '../../components';
 
 import { client, urlFor } from '../../lib/client';
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
+import { useStateContext } from '../../context/StateContext';
 
 const ProductDetails = ({product, products}) => {
     const {image, name, details, price} = product;
     const [index, setIndex] = useState(0);
+
+    const {increaseQuantity, decreaseQuantity, quantity, onAdd} = useStateContext();
+
   return (
     <div>
     <div className="product__detail-container">
@@ -48,13 +52,13 @@ const ProductDetails = ({product, products}) => {
         <div className="quantity">
           <h3>Quantity:</h3>
           <p className="quantity__desc">
-            <span className="minus" onClick=""><AiOutlineMinus /></span>
-            <span className="num">{1}</span>
-            <span className="plus" onClick=""><AiOutlinePlus /></span>
+            <span className="minus" onClick={decreaseQuantity}><AiOutlineMinus /></span>
+            <span className="num">{quantity}</span>
+            <span className="plus" onClick={increaseQuantity}><AiOutlinePlus /></span>
           </p>
         </div>
         <div className="buttons">
-          <button type="button" className="add-to-cart" onClick="">Add to Cart</button>
+          <button type="button" className="add-to-cart" onClick={() => onAdd(product, quantity)}>Add to Cart</button>
           <button type="button" className="buy-now" onClick="">Buy Now</button>
         </div>
       </div>
